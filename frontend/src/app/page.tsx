aa59a4c848be4fab30bfdf5d8ce51027a51cb71f
Leemuns <firedew123@gmail.com>
2026-07-13 05:22:03 +0800

    Test deploy

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,20 +4,20 @@ import { cookies } from "next/headers";
 import UserDashboard from "@/components/UserDashboard";
 
 export default async function Home() {
-  const cookieStore = await cookies();
-  const token = cookieStore.get("access_token");
+  // const cookieStore = await cookies();
+  // const token = cookieStore.get("access_token");
 
   return (
     <div>
       <h1>Home page</h1>
-      {token ? (
+      {/* {token ? (
         <>
           <Link href={"/loan-form/business"}>Apply for a new loan</Link>
           <UserDashboard />
         </>
       ) : (
         <Link href={"/login"}>Login to start applying</Link>
-      )}
+      )} */}
     </div>
   );
 }
