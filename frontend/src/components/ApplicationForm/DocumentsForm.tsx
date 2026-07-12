c9897e6c1c62d1ca243403762cf4e9490d174106
Leemuns <firedew123@gmail.com>
2026-07-13 04:18:18 +0800

    Fix deployment errors for vercel

--- frontend/src/components/ApplicationForm/DocumentsForm.tsx
+++ frontend/src/components/ApplicationForm/DocumentsForm.tsx
@@ -1,25 +1,23 @@
 "use client";
 
-import { useLoanStore } from "@/providers/LoanStoreProvider";
-import { useRouter } from "next/navigation";
-import { type SyntheticEvent } from "react";
+// import { useLoanStore } from "@/providers/LoanStoreProvider";
+// import { useRouter } from "next/navigation";
+// import { type SyntheticEvent } from "react";
 
 export default function DocumentsForm() {
-  const router = useRouter();
-  const { companyName, industryType, setField } = useLoanStore(
-    (state) => state,
-  );
-
-  const handleSubmit = (e: SyntheticEvent) => {
-    e.preventDefault();
-
-    router.push("/loan/requirements");
-  };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <p>Documents</p>
-      <button type="submit">Next</button>
-    </form>
-  );
+  // const router = useRouter();
+  // const { companyName, industryType, setField } = useLoanStore(
+  //   (state) => state,
+  // );
+  // const handleSubmit = (e: SyntheticEvent) => {
+  //   e.preventDefault();
+  //   router.push("/loan/requirements");
+  // };
+  // return (
+  //   <form onSubmit={handleSubmit}>
+  //     <p>Documents</p>
+  //     <button type="submit">Next</button>
+  //   </form>
+  // );
+  return <></>;
 }
